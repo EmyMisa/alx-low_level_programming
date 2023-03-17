d2700ccbcc07b93c38c4c15a178570aa7b877a11
Emelia <edboadimisa@gmail.com>
2023-03-16 18:31:16 -0700

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main function with phrase as standard input
+ *
+ * Return: result is 0 upon success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
