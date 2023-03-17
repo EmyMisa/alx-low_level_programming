d6714946a0e87f49efb849a6eb3c7508bbb545c2
Emelia <edboadimisa@gmail.com>
2023-03-16 19:15:40 -0700

    HHello printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - main function with phrase as standard input
+ *main - main function with standard I/O
  *
- * Return: result is 0 upon success
+ *Return: 0 upon success
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
 }
