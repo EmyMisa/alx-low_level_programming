306367e04cdc8f44fc5ca671a8a62e40c2702a8d
Emelia <edboadimisa@gmail.com>
2023-03-16 18:16:07 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
  * main - this is my main function with standard output
  *
  * Return: value is always 0
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
