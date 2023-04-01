fa6a2af1fea6c92a0fb4a89a630536d56e2e55d7
Emelia <edboadimisa@gmail.com>
2023-04-01 00:55:22 -0700

    program prints a[2] = 98

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -14,22 +14,16 @@ int main(void)
 
 	a[2] = 1024;
 	p = &n;
-  /**
+  /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
    * - you are not allowed to modify p
    * - only one statement
    * - you are not allowed to code anything else than this line of code
-   **/
-	*p = a[2] - 926;
-  /*
-   * main -prints to stdout
-   * subtracts 926 from the value of a[2]
-   * stores the result in the variable pointed to by p, which is n
-   * a[2] is initialized to 1024,subtracting 926 from it will result in 98
-   * return - 0
    */
+	*(p + 5) = 98;
+/*  prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
