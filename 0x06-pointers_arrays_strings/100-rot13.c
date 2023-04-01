f0837071314686989a46f6c4b102bc3086c99add
Emelia <edboadimisa@gmail.com>
2023-03-31 22:31:11 -0700

    a function that encodes a string using rot13 using prototype: char *rot13(char *)

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rot13 - encodes a string using rot13
+ *
+ * @s: the string to encode
+ *
+ * Return: a pointer to the encoded string
+ */
+
+char *rot13(char *s)
+{
+	char *p = s;
+
+	while (*p)
+	{
+		if ((*p >= 'a' && *p <= 'm') || (*p >= 'A' && *p <= 'M'))
+		*p += 13;
+		else if ((*p >= 'n' && *p <= 'z') || (*p >= 'N' && *p <= 'Z'))
+		*p -= 13;
+		p++;
+	}
+	return (s);
+}
+
